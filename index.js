1a50662a9201732e1aa5511f04f683ec89109dc8
Compass <daniktherrien@gmail.com>
2023-08-18 19:43:48 -0400

    added comments explaining the replace function

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const shapes = require('./lib/shapes');
 
 
 //This function will take all the information from the user and create the SVG file
+// the const shapeTemplate will take the shape from the shapes object and replace the ShapeColor with the color from the user
 function generateSVG(text, textColor, shape, shapeColor) {
   const shapeTemplate = shapes[shape].replace('ShapeColor', shapeColor);
   const svgContent = `
